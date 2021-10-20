0c95f1c7444b431dd989d0d8942962cc0bb38975
tdmitryny <tdmitryny@gmail.com>
2021-10-20 17:53:03 -0400

    Updated changes

--- scripts/script.js
+++ scripts/script.js
@@ -6,10 +6,10 @@ const seeMore = document.querySelector('.btn-more');
 
 
 
+
 //Drop down description
 const dropContent = function (e) {
     let clicked = e.target.closest('.read__more');
-    console.log(clicked);
     if (!clicked) return;
 
     //Add active arrow
@@ -45,91 +45,6 @@ const moreContent = function (e) {
 
 }
 seeMore.addEventListener('click', moreContent)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// boxesContainer.addEventListener('click', function (e) {
-//     let clicked = e.target.closest('.read__more');
-//     console.log(clicked);
-//     if (!clicked) return;
-
-//     //Add clear class
-//     reaMore.forEach(read => {
-//         read.classList.remove('active');
-//     });
-//     //Add active arrow
-//     clicked.classList.add('active');
-
-
-//     //Add hidden class
-//     description.forEach(c => {
-//         c.classList.add('hidden')
-//     })
-
-
-//     //Activate content
-//     document.querySelector(`.description--${clicked.dataset.tab}`).classList.remove('hidden');
-
-// });
-
-
-
-
-
-
-
-
-
-
-
-
-
-// reaMore.forEach(desc => {
-
-//     desc.addEventListener('click', function (e) {
-
-//         let readParent = e.target;
-//         console.log(readParent);
-
-//         if (description.classList.contains('hidden')) {
-//             description.classList.remove('hidden');
-//             setTimeout(() => {
-//                 description.classList.remove('visuallyhidden');
-//             }, 20)
-//             description.classList.remove('visuallyhidden');
-//         } else {
-//             description.classList.add('visuallyhidden');
-//             description.addEventListener('transitionend', function (e) {
-//                 e.preventDefault();
-//                 description.classList.add('hidden');
-//             }, {
-//                 capture: false,
-//                 once: true,
-//                 passive: false,
-//             });
-//         }
-
-//     }, false)
-
-// });
-
-
-
-
 
 
 
